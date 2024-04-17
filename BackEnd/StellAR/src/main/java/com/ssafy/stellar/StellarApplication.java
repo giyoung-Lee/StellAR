b4185f5a4132289254d7685ac5b9cefd012d7f1f
이기영 <rldud112@naver.com>
2024-04-17 11:35:27 +0900

    Test: merge test

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StellArApplication {
+public class StellarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StellArApplication.class, args);
+        SpringApplication.run(StellarApplication.class, args);
     }
 
 }

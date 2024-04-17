6127e71736996c843fcbb96924137342be3883f4
윤주찬 <wncks2546@naver.com>
2024-04-17 14:00:55 +0900

    Feat: Swagger 연결 테스트 설정

--- BackEnd/StellAR/src/test/java/com/ssafy/stellar/StellarApplicationTests.java
+++ BackEnd/StellAR/src/test/java/com/ssafy/stellar/StellarApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StellArApplicationTests {
+class StellarApplicationTests {
 
     @Test
     void contextLoads() {

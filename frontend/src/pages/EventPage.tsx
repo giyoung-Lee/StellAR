ff5a8671b88941e6aadef878aabe8de6e2c63459
josuhun <chosh9128@gmail.com>
2024-04-23 17:12:22 +0900

    Feat : 천문 현상 소식 컴포넌트 및 CSS

--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import * as p from './style/CommonPageStyle';
+import * as h from '../components/common/HeaderStyle';
+import EventList from '../components/Event/EventList';
 
 const EventPage = () => {
   return (
-    <div>EventPage</div>
-  )
-}
+    <p.Wrapper>
+      <h.Header>
+        <h.Title>천문 현상 소식</h.Title>
+      </h.Header>
+      <EventList />
+    </p.Wrapper>
+  );
+};
 
-export default EventPage
+export default EventPage;

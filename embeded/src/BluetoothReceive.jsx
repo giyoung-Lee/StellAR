b3ee9c72ed70d8e1ed58172fff686536c3aeeafd
김민영 <jnu195260@gmail.com>
2024-04-23 17:44:08 +0900

    Docs: update test bluetooth code

--- embeded/src/BluetoothReceive.jsx
+++ embeded/src/BluetoothReceive.jsx
@@ -1,45 +1,63 @@
 import { useEffect } from "react";
 
-const BluetoothReceive = () => {
+const BluetoothReceive = () => {    
+
+  // 블루투스 기기 연결
+  const connectBluetoothDevice = async () => {
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [
+          {
+            // services: [0xFFE0],
+            services: ['A4:75:B9:C9:25:4A'],
+          },
+        ],
+      });
+
+      const server = await device.gatt.connect();
+      const service = await server.getPrimaryService(0xFFE0);
+      const characteristic = await service.getCharacteristic(0xFFE1);
+
+      
+      await characteristic.startNotifications();
+      characteristic.addEventListener(
+        "characteristicvaluechanged",
+        handleCharacteristicValueChanged
+      );
+      console.log("블루투스 기기에 연결되었습니다.");
+    } catch (error) {
+      console.error("블루투스 연결 실패:", error);
+    }
+  };
+
+  // 데이터 처리 함수
+  const handleCharacteristicValueChanged = (event) => {
+    const value = event.target.value;
+    const receivedData = value.getInt8(0); // 8비트 정수로 데이터를 읽음
+    console.log("수신된 데이터:", receivedData);
+  };
+
   useEffect(() => {
-    // 블루투스 기기 연결
-    const connectBluetoothDevice = async () => {
-      try {
-        const device = await navigator.bluetooth.requestDevice({
-          filters: [
-            {
-              services: [0xFFE0],
-            },
-          ],
-        });
+    // connectBluetoothDevice();
+    const bluetoothButton=document.getElementById("bluetoothButton")
 
-        const server = await device.gatt.connect();
-        const service = await server.getPrimaryService(0xFFE0);
-        const characteristic = await service.getCharacteristic(0xFFE1);
+    bluetoothButton.addEventListener("click",()=>{
+      navigator.bluetooth.requestDevice({
+        filters: [{ services:["2C:CF:67:19:82:C8"]}]
+      })
+    })
 
-        
-        await characteristic.startNotifications();
-        characteristic.addEventListener(
-          "characteristicvaluechanged",
-          handleCharacteristicValueChanged
-        );
-        console.log("블루투스 기기에 연결되었습니다.");
-      } catch (error) {
-        console.error("블루투스 연결 실패:", error);
-      }
-    };
-
-    // 데이터 처리 함수
-    const handleCharacteristicValueChanged = (event) => {
-      const value = event.target.value;
-      const receivedData = value.getInt8(0); // 8비트 정수로 데이터를 읽음
-      console.log("수신된 데이터:", receivedData);
-    };
-
-    connectBluetoothDevice();
   }, []);
 
-  return <div>BluetoothReceive</div>;
+  
+
+  return (
+    <>
+  <div>BluetoothReceive</div>
+  <button id="bluetoothButton">블루투스 검색!</button>
+    </>  
+  )
+
 };
 
 export default BluetoothReceive;

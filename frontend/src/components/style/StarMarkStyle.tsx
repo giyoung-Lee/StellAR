830842d3e0c3fe3e3e7acb58657df83a1e41ae64
josuhun <chosh9128@gmail.com>
2024-04-23 16:15:30 +0900

    Feat : 별마크 컴포넌트 생성 및 CSS 적용

---
+++ frontend/src/components/style/StarMarkStyle.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const StarInfo = styled.p`
+  position: absolute;
+  width: 100%;
+  top: 20%;
+  left: 5%;
+  height: 55%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const StarName = styled.p`
+  color: var(--color-orange);
+  font-size: 17px;
+`;
+
+export const Date = styled.p`
+  color: var(--color-light);
+  font-size: 13px;
+`;
+
+export const Star = styled.span`
+  position: absolute;
+  right: 10%;
+  .container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .container {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .container svg {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 30px;
+    width: 30px;
+    transition: all 0.3s;
+    fill: #666;
+  }
+
+  .container svg:hover {
+    transform: scale(1.1);
+  }
+
+  .container input:checked ~ svg {
+    fill: #ffeb49;
+  }
+`;

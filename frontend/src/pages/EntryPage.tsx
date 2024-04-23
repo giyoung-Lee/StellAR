e17a0ae3842f52ac247e7e0949eab280b0fc2b10
Jiyong <aop8021@naver.com>
2024-04-23 13:54:43 +0900

    Feat: 기타 소소한 수정

--- frontend/src/pages/EntryPage.tsx
+++ frontend/src/pages/EntryPage.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import * as e from './style/EntryPageStyle';
+import logo from '/icons/logo_nobg.png';
 
 const EntryPage = () => {
-  return <div>EntryPage</div>;
+  return (
+    <>
+      <div>
+        <e.EntryImg src={logo} alt="logo_image" />
+      </div>
+      <div>
+        <p className='text-2xl text-white'>우하하하하하</p>
+        <button className='text-white'>별 보러 가기</button>
+      </div>
+    </>
+  );
 };
 
 export default EntryPage;
-
---
+++ frontend/src/pages/style/EntryPageStyle.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const EntryImg = styled.img`
+  width: 270px;
+  height: 220px;
+`;
+
+export const EntryBtn = styled.button`
+  border: 1px solid (#391764 0%, #ff00a8 50%, #ffe500 100%);
+  background-color: transparent;
+  color: white;
+`;

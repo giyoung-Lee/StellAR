0c7809ac635c39b96fb9ee951319fe452c1e551d
josuhun <chosh9128@gmail.com>
2024-04-24 17:38:44 +0900

    test : 3d 맵 테스트

---
+++ frontend/src/components/Star/StarMesh.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import * as THREE from 'three';
+import { getRandomInt } from '../../utils/random';
+import { useFrame } from '@react-three/fiber';
+
+type Props = {
+  position: THREE.Vector3;
+  size: number;
+};
+
+const StarMeshes = ({ position, size }: Props) => {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const COLOR = ['#88beff', 'white', '#f9d397', '#fd6b6b', '#ffffac'];
+  const colorIndex = getRandomInt(0, COLOR.length);
+  const Y_AXIS = new THREE.Vector3(0, 1, 0);
+  const DIST_LIMIT = 10000;
+
+  useFrame((state, delta) => {
+    const pos = meshRef.current.position.applyAxisAngle(Y_AXIS, delta / 100);
+    const dist = state.camera.position.distanceTo(meshRef.current.position);
+    if (dist > DIST_LIMIT)
+      meshRef.current.scale.set(
+        dist / DIST_LIMIT,
+        dist / DIST_LIMIT,
+        dist / DIST_LIMIT,
+      );
+    meshRef.current.position.x = pos.x;
+    meshRef.current.position.y = pos.y;
+    meshRef.current.position.z = pos.z;
+  });
+
+  return (
+    <mesh ref={meshRef} position={position}>
+      <sphereGeometry args={[size, 32, 16]} />
+      <meshStandardMaterial color={COLOR[colorIndex]} />
+    </mesh>
+  );
+};
+
+export default StarMeshes;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import StarInfoCarousel from '../components/StarInfoCarousel/StarInfoCarousel';
 import * as h from './style/HomePageStyle';
+import MainCanvas from '../components/Star/MainCanvas';
 
 const HomePage = () => {
   return (
     <>
       <h.Wrapper>
-        <StarInfoCarousel active={0} />
+        {/* <StarInfoCarousel active={0} /> */}
+        <MainCanvas />
       </h.Wrapper>
     </>
   );
 };
 
 export default HomePage;
-

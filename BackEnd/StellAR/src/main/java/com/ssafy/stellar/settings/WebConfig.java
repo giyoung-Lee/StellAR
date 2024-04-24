e0009f7ecbf25b41acfff412883b29130ae05bcf
윤주찬 <wncks2546@gmail.com>
2024-04-25 01:00:40 +0900

    Feat: 비밀번호 암호화 설정

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/settings/WebConfig.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/settings/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.stellar.settings;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,8 +12,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+    /**
+     * 리다이렉트 url 설정
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
+    }
+
 }

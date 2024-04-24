e136747a7470f09558bd4d53434fcd5e068a419a
윤주찬 <wncks2546@naver.com>
2024-04-24 14:51:41 +0900

    Feat: 누락된 파일 추가

---
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/user/dto/request/SignUpDto.java
@@ -0,0 +1,36 @@
+package com.ssafy.stellar.user.dto.request;
+
+import com.ssafy.stellar.user.entity.UserEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class SignUpDto {
+
+    @Schema(description = "사용자 ID", required = true)
+    private String userId;
+
+    @Schema(description = "비밀번호", required = true)
+    private String password;
+
+    @Schema(description = "사용자 이름", required = false)
+    private String name;
+
+    @Schema(description = "성별", required = false)
+    private String gender;
+
+    public UserEntity toEntity() {
+        return UserEntity.builder()
+                .userId(this.userId)
+                .password(this.password)
+                .name(this.name)
+                .build();
+    }
+
+}
+
+

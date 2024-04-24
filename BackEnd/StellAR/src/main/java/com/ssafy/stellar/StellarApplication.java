9cf1f2996715d7c4d709c6a4e215322d7ea799db
이기영 <rldud112@naver.com>
2024-04-24 18:00:49 +0900

    Fix: 별데이터 저장

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.stellar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StellarApplication {
 
     public static void main(String[] args) {

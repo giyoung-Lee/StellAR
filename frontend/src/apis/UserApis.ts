b30152d7000c3692c23650ed981a13b41a00a6a6
Jiyong <aop8021@naver.com>
2024-04-24 17:59:57 +0900

    Feat: 회원가입 로그인 폼데이터 형식으로 변경

--- frontend/src/apis/UserApis.ts
+++ frontend/src/apis/UserApis.ts
@@ -3,8 +3,12 @@ import { publicRequest } from '../hooks/requestMethods';
 // POST 요청
 // 로그인
 export const loginApi = async (loginData: loginApiType) => {
+  const formData = new FormData();
+  formData.append('userId', loginData.userId);
+  formData.append('password', loginData.password);
+
   return publicRequest
-    .post(`user/login`, loginData)
+    .post(`user/login`, formData)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);
@@ -12,10 +16,15 @@ export const loginApi = async (loginData: loginApiType) => {
     });
 };
 
+
 // 회원가입
 export const signupApi = async (signupData: signupApiType) => {
+  const formData = new FormData();
+  formData.append('userId', signupData.userId);
+  formData.append('password', signupData.password);
+
   return publicRequest
-    .post(`user/signup`, signupData)
+    .post(`user/signup`, formData)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);

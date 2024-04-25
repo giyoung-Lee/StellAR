0229563fe362eebfcf27271f8b4f6270bf2b160f
이기영 <rldud112@naver.com>
2024-04-25 15:55:39 +0900

    Feat: 로그 파일설정 완료!

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/utils/stars/StarInfo.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/utils/stars/StarInfo.java
@@ -46,20 +46,21 @@ public class StarInfo {
             String[] parts = input.split("\\|");
             return new ArrayList<>(Arrays.asList(parts));
         } else {
-            System.out.println("Processed whole string: " + input);
             List<String> list = new ArrayList<>();
             list.add(input);
             return list;
         }
     }
 
-    @Async
     public void saveStars(List<String[]> starList) {
+        int count = 0;
+
         // 0.Identifier     1.RA            2.DEC           3.PMRA      4.PMDEC
         // 5.Mag_V          6.Parallax      7.SP_TYPE 	    8.Name      9.Constellation
         // 10.HD	        11.SAO	        12.HIP	        13.HR	    14.Gaia DR2
         // 15.Gaia DR3      16.Star_Type
         for (String[] star : starList) {
+            count ++;
             Star starEntity = new Star();
 
             List<String> temp = null;
@@ -71,8 +72,6 @@ public class StarInfo {
                 // | 있는지 확인 해서 뜯어서 저장해 주고,
                 temp = processString(star[9]);
                 // 나머지 번호에 맞는 이름으로 해줌...
-            } else if (!star[10].isEmpty()) {
-                starEntity.setStarId(star[10]);
             } else if (!star[11].isEmpty()) {
                 starEntity.setStarId(star[11]);
             } else if (!star[12].isEmpty()) {
@@ -86,9 +85,20 @@ public class StarInfo {
             }
             starEntity.setRA(star[1]);
             starEntity.setDeclination(star[2]);
-            starEntity.setPMRA(star[3]);
-            starEntity.setPMDEC(star[4]);
-            starEntity.setMagV(star[5]);
+            if (star[3] == null || star[3].equals("--") || star[3].isEmpty()) {
+                starEntity.setPMRA("0");
+                starEntity.setPMDEC("0");
+            } else {
+                starEntity.setPMRA(star[3]);
+                starEntity.setPMDEC(star[4]);
+            }
+
+            if (star[5] == null || star[5].equals("--") || star[5].isEmpty()) {
+                starEntity.setMagV("F");
+            } else {
+                starEntity.setMagV(star[5]);
+            }
+
             starEntity.setParallax(star[6]);
             starEntity.setSP_TYPE(star[7]);
             starEntity.setHD(processString(star[10]).get(0));
@@ -100,20 +110,28 @@ public class StarInfo {
                     starEntity.setStarId(constellation);
                     starRepository.save(starEntity);
                 }
+            } else {
+                if (starEntity.getStarId() == null) {
+                   starEntity.setStarId(star[0]);
+                }
+               starEntity.setConstellation("");
+               starRepository.save(starEntity);
             }
+            System.out.println(count + " / " + starList.size());
         }
     }
 
-    @PostConstruct
-    public void init() {
-        try {
-            List<String[]> starData = readCSVToArray();
-            saveStars(starData);
-            System.out.println("Data successfully loaded and saved.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error loading data from CSV.");
-        }
-    }
+    // 별 정보 db에 넣을거면 주석을 풀고 StellarApplication을 한번 실행 하세요
+//    @PostConstruct
+//    public void init() {
+//        try {
+//            List<String[]> starData = readCSVToArray();
+//            saveStars(starData);
+//            System.out.println("Data successfully loaded and saved.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("Error loading data from CSV.");
+//        }
+//    }
 }
 

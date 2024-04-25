bd3ed56d78696284c26a147ef0abf7ddf1cfaf24
Jiyong <aop8021@naver.com>
2024-04-25 17:57:27 +0900

    Feat: Navbar 구현 시도

---
+++ frontend/src/components/common/NavBar/NavIcon.tsx
@@ -0,0 +1,14 @@
+import './NavIcon.css';
+
+const NavBar = () => {
+  return (
+    <div className="fixed flex justify-center min-w-full bottom-5">
+      <div className="checkbox-wrapper">
+        <input id="_checkbox-26" type="checkbox" />
+        <label htmlFor="_checkbox-26"></label>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;

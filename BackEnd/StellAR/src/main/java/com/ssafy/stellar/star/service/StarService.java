64207b651c8a6c66f2580cf7fcc573a9ef72f30c
이기영 <rldud112@naver.com>
2024-04-29 11:43:54 +0900

    Fix: log 에러 수정

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/star/service/StarService.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/star/service/StarService.java
@@ -1,11 +1,10 @@
 package com.ssafy.stellar.star.service;
 
-import com.ssafy.stellar.star.dto.response.StarReturnAllDto;
-import com.ssafy.stellar.star.entity.StarEntity;
+import com.ssafy.stellar.star.dto.response.StarDto;
 
 import java.util.List;
 
 public interface StarService {
 
-    public List<StarReturnAllDto> returnAllStar();
+    public List<StarDto> returnAllStar();
 }

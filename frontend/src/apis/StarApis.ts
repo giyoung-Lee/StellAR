5e77edfe99ed1739660638543524c9a35ff2b6d1
josuhun <chosh9128@gmail.com>
2024-04-30 14:27:20 +0900

    Feat : constellationo 호출 api 연결

--- frontend/src/apis/StarApis.ts
+++ frontend/src/apis/StarApis.ts
@@ -3,3 +3,11 @@ import { publicRequest } from '../hooks/requestMethods';
 export const GetStars = async () => {
   return await publicRequest.get('star/all');
 };
+
+export const GetConstellation = async (type: string) => {
+  return await publicRequest.get('constellation/link', {
+    params: {
+      constellationType: type,
+    },
+  });
+};

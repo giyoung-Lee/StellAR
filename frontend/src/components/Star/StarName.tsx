06ceef43c2584cc5f027ee28a405a2a8aa9f5679
josuhun <chosh9128@gmail.com>
2024-04-30 09:29:13 +0900

    Style : name컴포넌트 클릭이벤트 제거

--- frontend/src/components/Star/StarName.tsx
+++ frontend/src/components/Star/StarName.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import useStarStore from '../../stores/starStore';
 import * as n from '../style/StarNameStyle';
 
 const StarName = () => {
   const starStore = useStarStore();
-  return <n.Wrapper>{starStore.starId}</n.Wrapper>;
+  const starNameRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        starNameRef.current &&
+        !starNameRef.current.contains(event.target as Node)
+      ) {
+        starStore.setStarClicked(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [starStore]);
+
+  return <n.Wrapper ref={starNameRef}>{starStore.starId}</n.Wrapper>;
 };
 
 export default StarName;

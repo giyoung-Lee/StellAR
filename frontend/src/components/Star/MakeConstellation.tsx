2eb9149f3e71e11a3dee60955529fa9a89f78dc0
josuhun <chosh9128@gmail.com>
2024-05-03 15:40:11 +0900

    Style : 별자리 선 메쉬 변경

--- frontend/src/components/Star/MakeConstellation.tsx
+++ frontend/src/components/Star/MakeConstellation.tsx
@@ -1,10 +1,13 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
+import { useThree, extend } from '@react-three/fiber';
 import * as THREE from 'three';
+import { TubeGeometry } from 'three';
 import { Line2 } from 'three/examples/jsm/lines/Line2.js';
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
+import { Vector3, CatmullRomCurve3 } from 'three';
 
-import { useThree } from '@react-three/fiber';
+extend({ Line2, LineGeometry, LineMaterial });
 
 type Props = {
   constellation: string;
@@ -13,39 +16,29 @@ type Props = {
 };
 
 const MakeConstellation = ({ constellation, pointA, pointB }: Props) => {
-  const lineRef = useRef<Line2>(null);
-
+  const lineRef = useRef<THREE.BufferGeometry | null>(null);
   const { scene } = useThree();
 
   const handleClick = () => {
     console.log('Line clicked:', constellation);
   };
 
-  return (
-    <line
-    //   ref={meshRef}
-    // position={pointA}
-    // castShadow={false}
-    // receiveShadow={false}
-    >
-      <bufferGeometry
-        attach="geometry"
-        ref={(geometry) => {
-          if (geometry && pointA && pointB) {
-            const vectorA = new THREE.Vector3(pointA.x, pointA.y, pointA.z);
-            const vectorB = new THREE.Vector3(pointB.x, pointB.y, pointB.z);
-            const points = [vectorA, vectorB];
-            geometry.setFromPoints(points);
-            geometry.setAttribute(
-              'lineWidth',
-              new THREE.Float32BufferAttribute([1000, 1000], 1),
-            );
-          }
-        }}
-      />
+  // 두꺼운 클릭 영역을 생성하기 위한 투명 튜브
+  const path = new CatmullRomCurve3([pointA, pointB]);
+  const tubeGeometry = new TubeGeometry(path, 20, 130, 8, false);
+  const tubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    opacity: 0.2,
+    transparent: true,
+  });
 
-      <lineBasicMaterial color={'white'} />
-    </line>
+  return (
+    <mesh geometry={tubeGeometry} material={tubeMaterial} onClick={handleClick}>
+      <line>
+        <bufferGeometry attach="geometry" ref={lineRef} />
+        <lineBasicMaterial color={'white'} />
+      </line>
+    </mesh>
   );
 };
 

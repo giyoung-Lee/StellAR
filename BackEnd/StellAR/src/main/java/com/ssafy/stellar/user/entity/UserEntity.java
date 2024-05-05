350ad84793be423a4c8eb35e4e49cbcf84a4cbbc
윤주찬 <wncks2546@gmail.com>
2024-05-05 16:41:15 +0900

    Test: 유저 단위테스트 코드 작성

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/user/entity/UserEntity.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/user/entity/UserEntity.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 @Entity(name = "user_info")
 public class UserEntity {
 
--- BackEnd/StellAR/src/test/java/com/ssafy/stellar/user/service/UserServiceImplTest.java
+++ BackEnd/StellAR/src/test/java/com/ssafy/stellar/user/service/UserServiceImplTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserServiceImplTest {
+class UserServiceTest {
 
     @Test
     void signUp() {

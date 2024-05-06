a4199b8b8ecea98c26696c7819c3d4ed2d07bfcf
이기영 <rldud112@naver.com>
2024-05-06 22:22:47 +0900

    Fix: CORS 살짝

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/StellarApplication.java
@@ -3,12 +3,17 @@ package com.ssafy.stellar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+<<<<<<< Updated upstream
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+=======
+import org.springframework.scheduling.annotation.EnableScheduling;
+>>>>>>> Stashed changes
 
 @EnableScheduling
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+@EnableScheduling
 public class StellarApplication {
     public static void main(String[] args) {
         SpringApplication.run(StellarApplication.class, args);

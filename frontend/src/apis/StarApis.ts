11db429f8b643ddd4da02b30ece48a059e1fca02
Jiyong <aop8021@naver.com>
2024-05-07 00:41:08 +0900

    Feat: 나만의 별자리 관련 API 작성

--- frontend/src/apis/StarApis.ts
+++ frontend/src/apis/StarApis.ts
@@ -28,3 +28,33 @@ export const GetPlanets = async () => {
   return await publicRequest.get('star/planet');
 };
 
+export const GetUserConstellation = async (userId: string) => {
+  return await publicRequest.get('user-constellation/all', {
+    params: {
+      userId: userId,
+    },
+  })
+};
+
+export const MakeUserConstellation = async (userConstellationData: userConstellationApiType) => {
+  const formData = new FormData();
+  formData.append('userId', userConstellationData.userId);
+  formData.append('name', userConstellationData.name);
+  formData.append('description', userConstellationData.description);
+  // 'links'가 배열이기 때문에 JSON 문자열로 변환하여 추가
+  if (userConstellationData.links) {
+    formData.append('links', JSON.stringify(userConstellationData.links));
+  }
+
+  return await publicRequest
+    .post(`user-constellation/create`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log(error);
+      throw new Error('나만의 별자리 만들기 api 에러');
+    });
+};

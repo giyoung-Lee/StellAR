821fe6ee10543bb30fdc0eaf37e635eb09939899
이기영 <rldud112@naver.com>
2024-05-06 20:58:19 +0900

    Test: 쟌긴스 테스트

--- frontend/src/hooks/requestMethods.tsx
+++ frontend/src/hooks/requestMethods.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://k10c105.p.ssafy.io:8080/api/';
+const BASE_URL = 'https://k10c105.p.ssafy.io:8080/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,

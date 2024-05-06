c7f1d459d17dfcab7ece74ed03d3e2c47bfc8f2e
이기영 <rldud112@naver.com>
2024-05-06 23:31:03 +0900

    Test: cors

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/settings/SecurityConfig.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/settings/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.ssafy.stellar.settings;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final String releaseHostName ;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -22,8 +30,28 @@ public class SecurityConfig {
         httpSecurity
                 .httpBasic(http -> http.disable())  // HTTP 기본 인증 비활성화
                 .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-                .cors(cors -> cors.disable())  // CORS 설정 비활성화
-                .authorizeHttpRequests(auth -> auth
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("https://"+releaseHostName));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+
+                        return configuration;
+                    }
+                }));
+
+        httpSecurity.authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll());  // 모든 요청에 대해 접근 허용
 
         return httpSecurity.build();

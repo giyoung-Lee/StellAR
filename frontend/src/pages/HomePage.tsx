395a50a360443bad45088b4df16d28a300144d17
Jiyong <aop8021@naver.com>
2024-05-09 17:24:22 +0900

    Feat: 글래스모피즘 버튼 생성

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import MainCanvas from '../components/Star/MainCanvas';
 import useStarStore from '../stores/starStore';
 import StarName from '../components/Star/StarName';
 import React, { useEffect } from 'react';
-import MarkBtn from '../components/StarMark/MarkBtn';
 import { useQuery } from '@tanstack/react-query';
 import { GetStarMark } from '../apis/StarMarkApis';
 import useUserStore from '../stores/userStore';
@@ -48,14 +47,28 @@ const HomePage = () => {
   return (
     <>
       <h.Wrapper>
-        {starStore.starClicked ||
-        starStore.planetClicked ||
-        starStore.zoomFromOther ? (
+        {(starStore.starClicked && starStore.linkedStars.length < 1) ||
+        (starStore.planetClicked && starStore.linkedStars.length < 1) ||
+        (starStore.zoomFromOther && starStore.linkedStars.length < 1) ? (
           <StarName />
         ) : null}
+
+        {starStore.linkedStars.length > 0 ? (
+          <div className="absolute flex flex-col justify-between h-[200px] z-[1000]">
+            <button className="p-3 bg-white bg-opacity-25 border border-white rounded-xl shadow-custom border-opacity-18 backdrop-blur-sm">
+              나만의 별자리 생성
+            </button>
+
+            <button className="p-3 bg-white bg-opacity-25 border border-white rounded-xl shadow-custom border-opacity-18 backdrop-blur-sm">
+              다시 선택하기
+            </button>
+          </div>
+        ) : null}
+
         {constellationStore.constellationClicked ? (
           <StarInfoCarousel active={0} />
         ) : null}
+
         <MainCanvas />
       </h.Wrapper>
     </>

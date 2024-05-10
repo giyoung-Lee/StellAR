4cef2590f4694699afc5d87baf845c02ef9abd91
윤주찬 <wncks2546@naver.com>
2024-05-10 10:39:50 +0900

    Feat: 프론트엔드 FCM 세팅

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  
+  // 환경 변수 로딩
+  // EC2
+  const env = loadEnv(mode, path.resolve('/'));
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': {...env}
+    }
+  };
+
+  // 로컬
+  // const env = loadEnv(mode, process.cwd(), '');
+
+  // return {
+  //   plugins: [react()],
+  //   define: {
+  //     'process.env': env
+  //   }
+  // };
+});

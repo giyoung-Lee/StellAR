d1a633dfe42d9e0686036a4ae503393afc8ca9b4
윤주찬 <wncks2546@naver.com>
2024-05-10 15:32:42 +0900

    Fix : 어제 코드로 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,11 @@
-// vite.config.ts
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig(({ mode }) => {
-  
-  // 환경 변수 로딩
-  // EC2
-  const env = loadEnv(mode, path.resolve('/'));
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': {...env}
-    }
-  };
-
-  // 로컬
-  // const env = loadEnv(mode, process.cwd(), '');
-
-  // return {
-  //   plugins: [react()],
-  //   define: {
-  //     'process.env': env
-  //   }
-  // };
-});
+{
+  "compilerOptions": {
+    "composite": true,
+    "skipLibCheck": true,
+    "module": "ESNext",
+    "moduleResolution": "bundler",
+    "allowSyntheticDefaultImports": true,
+    "strict": true
+  },
+  "include": ["vite.config.ts"]
+}

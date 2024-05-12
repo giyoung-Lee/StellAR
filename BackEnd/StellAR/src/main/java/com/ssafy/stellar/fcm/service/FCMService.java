9cc9412686ae1db6d3d610b6ae1409242823a2cd
윤주찬 <wncks2546@gmail.com>
2024-05-12 23:33:06 +0900

    Test: 디바이스 토큰 저장 코드 작성

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/fcm/service/FCMService.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/fcm/service/FCMService.java
@@ -1,40 +1,11 @@
 package com.ssafy.stellar.fcm.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import com.ssafy.stellar.user.repository.UserRepository;
+import com.ssafy.stellar.user.entity.UserEntity;
 import org.springframework.stereotype.Service;
 
+public interface FCMService {
 
-@Service
-public class FCMService {
+    void sendNotification(String token, String title, String body);
 
-    private final FirebaseMessaging firebaseMessaging;
-    private final UserRepository userRepository;
-
-    FCMService(FirebaseMessaging firebaseMessaging,
-               UserRepository userRepository) {
-        this.firebaseMessaging = firebaseMessaging;
-        this.userRepository = userRepository;
-    }
-
-    public void sendNotification(String token, String title, String body) {
-        try {
-            Notification notification = Notification.builder()
-                    .setTitle(title)
-                    .setBody(body)
-                    .build();
-
-            Message message = Message.builder()
-                    .setToken(token)
-                    .setNotification(notification)
-                    .build();
-
-            firebaseMessaging.send(message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+    void saveDeviceToken(String deviceToken, UserEntity user);
 }

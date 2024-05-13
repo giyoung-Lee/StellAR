ee9a939b59dceac88189f5298a4a94ed8e9c28d5
윤주찬 <wncks2546@gmail.com>
2024-05-14 00:46:54 +0900

    Test: 유저 별자리 Entity 테스트코드 작성

--- BackEnd/StellAR/src/main/java/com/ssafy/stellar/fcm/service/FCMServiceImpl.java
+++ BackEnd/StellAR/src/main/java/com/ssafy/stellar/fcm/service/FCMServiceImpl.java
@@ -5,6 +5,7 @@ import com.ssafy.stellar.constellation.entity.ConstellationEventEntity;
 import com.ssafy.stellar.constellation.repository.ConstellationEventRepository;
 import com.ssafy.stellar.fcm.entity.DeviceTokenEntity;
 import com.ssafy.stellar.fcm.repository.DeviceTokenRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -49,7 +50,7 @@ public class FCMServiceImpl {
 
         Notification notification = Notification.builder()
                 .setTitle(title)
-                .setBody(body)
+                .setBody(body.replace("\\n", "\n"))
                 .build();
 
         MulticastMessage message = MulticastMessage.builder()
@@ -73,11 +74,8 @@ public class FCMServiceImpl {
         }
     }
 
-    public void scheduleMessage(Instant when, String title, String body) {
-        scheduler.schedule(() -> sendNotification(title, body), when);
-    }
-    // 매일 특정 시간에 실행
-
+    @PostConstruct
+    @Scheduled(cron = "0 0 0 * * ?")
     public void autoSetPushAlarm() {
         LocalDate today = LocalDate.now();
         LocalDate tomorrow = today.plusDays(1);
@@ -87,13 +85,24 @@ public class FCMServiceImpl {
         for (ConstellationEventEntity event : events) {
             String token = "your_device_token"; // 토큰 가져오는 로직 필요
             String title = event.getAstroEvent();
-            String body = "Don't forget your event at " + event.getAstroTime();
+
+            String body = event.getAstroEvent() + chooseParticle(event.getAstroEvent()) + "한 시간 뒤에 발생합니다." +
+                    "\n" + "관측이 힘든 사람들은 stellAR에서 함께 확인해요!";
 
             // 스케줄링 시간 계산 (예: 이벤트 시간 1시간 전)
             ZonedDateTime eventTime = ZonedDateTime.of(event.getLocdate(), LocalTime.parse(event.getAstroTime()), ZoneId.systemDefault());
             Instant scheduledTime = eventTime.minusHours(1).toInstant();
 
             scheduler.schedule(() -> sendNotification(title, body), scheduledTime);
+        }
+    }
+
+    public String chooseParticle(String word) {
+        char lastChar = word.charAt(word.length() - 1); // 단어의 마지막 문자
+        if ((lastChar - 0xAC00) % 28 > 0) {
+            return "이"; // 받침이 있을 경우
+        } else {
+            return "가"; // 받침이 없을 경우
         }
     }
 }

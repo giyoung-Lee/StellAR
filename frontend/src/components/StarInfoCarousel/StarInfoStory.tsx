e38d0f93c374fc61c71188ca9ce3380a6a271c87
Jiyong <aop8021@naver.com>
2024-05-14 00:11:29 +0900

    Refactor: 불필요한 코드삭제

--- frontend/src/components/StarInfoCarousel/StarInfoStory.tsx
+++ frontend/src/components/StarInfoCarousel/StarInfoStory.tsx
@@ -40,21 +40,6 @@ const StarInfoStory = ({ constellationData, setModalOpen }: Props) => {
       <s.CardHeader>
         {constellationData?.constellationSubName} 이야기
       </s.CardHeader>
-      <s.QuizBox>
-        <p className="q">
-          Q. 어쩌고저쩌고 자리는 어쩌고저쩌고인 그런 그런것이다. 질문이 별로
-          안길수도있지만 길수도있다.
-        </p>
-        <div className="answer_box">
-          <button className="o" onClick={handleError}>
-            <FontAwesomeIcon icon="o" color="white" className="icon" />
-          </button>
-
-          <button className="x" onClick={handleSuccess}>
-            <FontAwesomeIcon icon="xmark" color="white" className="icon" />
-          </button>
-        </div>
-      </s.QuizBox>
       <s.Mythology>{constellationData?.constellationStory}</s.Mythology>
     </s.CardWrapper>
   );

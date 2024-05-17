cef3478100125c6a7b0e5e217bd88ad0f366d038
윤주찬 <wncks2546@naver.com>
2024-05-17 14:21:46 +0900

    Fix : 돌리고 돌리고~

--- frontend/src/apis/MyConstApis.ts
+++ frontend/src/apis/MyConstApis.ts
@@ -1,13 +1,9 @@
 import { publicRequest } from '../hooks/requestMethods';
 import Swal from 'sweetalert2';
-import useConstellationStore from '../stores/constellationStore';
-import useStarStore from '../stores/starStore';
 
 export const MakeMyConstellationApi = async (
   userConstellationData: UserConstellationData,
 ) => {
-  const constellationStore = useConstellationStore();
-  const starStore = useStarStore();
   try {
     const res = await publicRequest.post(
       `user-constellation/create`,
@@ -24,12 +20,7 @@ export const MakeMyConstellationApi = async (
       }
     });
 
-    constellationStore.setConstellationClicked(false);
-    constellationStore.setConstellationName('');
-    starStore.setPlanetClicked(false);
-    starStore.setStarClicked(false);
-    starStore.setZoomFromOther(false);
-
+    window.location.reload();
     return res.data;
   } catch (error) {
     console.log(error);

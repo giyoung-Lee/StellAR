e29b5752fc6aee598c212ab176e87663cf77e454
josuhun <chosh9128@gmail.com>
2024-05-18 11:15:19 +0900

    Fix : 메인캔버스 로딩페이지 호출 수정

--- frontend/src/components/common/Loading/Loading.tsx
+++ frontend/src/components/common/Loading/Loading.tsx
@@ -1,9 +1,16 @@
 import './Loading.css';
 import { getRandomInt } from '../../../utils/random';
-import pieceKnowledge from '../../../assets/piece_knowledge.json'
+import pieceKnowledge from '../../../assets/piece_knowledge.json';
+import { useEffect, useState } from 'react';
 
 const Loading = () => {
-  const loadingMessage = pieceKnowledge.piece_knowledge
+  const [loadingMessage, setLoadingMessage] = useState('');
+
+  useEffect(() => {
+    const messages = pieceKnowledge.piece_knowledge;
+    const randomIndex = getRandomInt(0, messages.length - 1);
+    setLoadingMessage(messages[randomIndex]?.contents);
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[100vh]">
@@ -16,14 +23,12 @@ const Loading = () => {
           <span className="shadow"></span>
         </div>
       </div>
-
       <div className="absolute flex flex-col items-center justify-center mt-5 text-center">
-        <p className="message">
-          {loadingMessage ? loadingMessage[getRandomInt(0, 6)]?.contents : null}
-        </p>
+        <p className="message">{loadingMessage}</p>
       </div>
     </div>
   );
 };
 
 export default Loading;
+

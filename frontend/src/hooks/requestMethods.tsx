4efaca4756218b1e859123877b2027c5eb255e77
josuhun <chosh9128@gmail.com>
2024-05-20 04:22:53 +0900

    Fix : requestmethods 수정

--- frontend/src/hooks/requestMethods.tsx
+++ frontend/src/hooks/requestMethods.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8080/api/';
+const BASE_URL = 'http://localhost:8080/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,

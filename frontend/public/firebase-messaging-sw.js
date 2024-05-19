5afa377c83b40f53614b178c99d80b0f3705f751
윤주찬 <wncks2546@gmail.com>
2024-05-19 21:50:30 +0900

    Fix : FCM 중복 등록되는 설정 수정

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,27 +1,14 @@
-// public/firebase-messaging-sw.js
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDO-oXXwpueTV4p2LF_lK6UJFeWXXQf6_E',
-  authDomain: 'stellar-e2012.firebaseapp.com',
-  projectId: 'stellar-e2012',
-  storageBucket: 'stellar-e2012.appspot.com',
-  messagingSenderId: '400069443108',
-  appId: '1:400069443108:web:11fe7fac82f9b9e78862b1',
-  measurementId: 'G-MWYW1NJBNG'
-};
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
 
-// Firebase 앱 초기화
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message:', payload);
-  const notificationTitle = payload.notification?.title || 'Background Message Title';
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
   const notificationOptions = {
-    body: payload.notification?.body || 'Background Message Body',
+    body: resultData.body,
     icon: '/firebase-logo.png'
   };
 
@@ -29,17 +16,15 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 self.addEventListener("install", function (e) {
-  // console.log("fcm sw install..");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function (e) {
-  // console.log("fcm sw activate..");
+  console.log("Service Worker activated.");
 });
 
-// 어디로 라우팅 할지 설정
 self.addEventListener("notificationclick", function (event) {
-  console.log("notification click");
+  console.log("Notification click received.");
   const url = "/event";
   event.notification.close();
   event.waitUntil(clients.openWindow(url));

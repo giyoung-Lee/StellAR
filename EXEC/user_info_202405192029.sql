7dde0da10951b1cb82e863ff165406abbb02bc60
이기영 <rldud112@naver.com>
2024-05-19 20:32:17 +0900

    Docs: db dump file add

---
+++ EXEC/user_info_202405192029.sql
@@ -0,0 +1,13 @@
+INSERT INTO s10p31c105.user_info (user_name,user_password,user_gender,user_id) VALUES
+	 (NULL,'$2a$10$Abh07HrCbCicFowrbANAkeOwml17Ufof.P4T.0C8xP5mlzx0WpmBK',NULL,'a'),
+	 (NULL,'$2a$10$LsbDWpJaFr5uzM0O6gH29.4HpPFnPlsIUzEWrI3uJScEZ/xcnm2VK',NULL,'asd'),
+	 (NULL,'$2a$10$luoACK28Iu7aKzQ15CVNZe0bFWVTqrq9oiZCY5G9QPBaEAiQVydgS',NULL,'embId'),
+	 (NULL,'$2a$10$Mm2QYfH66rXo7a/0vD2I0eEXTYW9IQLzAmWlGAVnN6aIoAf.yeIcO',NULL,'minzero'),
+	 (NULL,'$2a$10$k/XAIhQAsOvP/ioUV1Kyh.ouZfKfHkuIVhkkFtHJOZj0rnhkDhFMy',NULL,'qqqwww'),
+	 (NULL,'$2a$10$FQM0Eg9wQsD9eN9QLlw8tOInlmE2S1KWRf5NzYxCkbdFvN9a33ITW',NULL,'qqqwwweee'),
+	 (NULL,'$2a$10$O1NZLtdzxF41qP2QCHxyXua//joTI6JYAZSI2FoSAwSf0uqQV9xRe',NULL,'qwe'),
+	 (NULL,'$2a$10$qAaNSpGhTdv3ed2QQEkm7e/Udj1wsbd.czR6Amx6BsJHqIs0kDvx6',NULL,'ssafy1'),
+	 ('김싸피','$2a$10$LOfh.lMc0QkgJS1bRh9zeearGeXuIfJdPf8BaaRQRbo5FqnBOpE4G','habu','test'),
+	 (NULL,'$2a$10$q6bRSdgK/pX0c6KFhWdZr.wCVQcpAD983RidkcEiZcinBvw/Pg1Dq',NULL,'wncks');
+INSERT INTO s10p31c105.user_info (user_name,user_password,user_gender,user_id) VALUES
+	 (NULL,'$2a$10$nUMKoumNFSHrN2ori8KL1Om2S.KVEVNmHkLSvGU0G4xpj8sZ05SFO',NULL,'wncks1');

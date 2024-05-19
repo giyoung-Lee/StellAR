2527ec6f59ec609d8d82fc2036aa97a9a36ecd77
이기영 <rldud112@naver.com>
2024-05-19 15:51:06 +0900

    Fix: url 진짜 수정

--- frontend/src/hooks/requestMethods.tsx
+++ frontend/src/hooks/requestMethods.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://k10c105.p.ssafy.io/api/';
+const BASE_URL = 'https://k10c105.p.ssafy.io/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
